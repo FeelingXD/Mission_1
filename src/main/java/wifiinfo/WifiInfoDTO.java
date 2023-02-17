073af7ebf015c25ee863dc111c141b173609bb97
FeelingXD <wlals425315@gmail.com>
2023-02-17 14:53:34 +0900

    Fix: add missed column

--- src/main/java/wifiinfo/WifiInfoDTO.java
+++ src/main/java/wifiinfo/WifiInfoDTO.java
@@ -11,6 +11,7 @@ public class WifiInfoDTO {
     private  String X_SWIFI_WRDOFC;
     private  String X_SWIFI_ADRES1;
     private  String X_SWIFI_ADRES2;
+    private String X_SWIFI_INSTL_FLOOR;
     private  String X_SWIFI_INSTL_MBY;
     private  String X_SWIFI_INSTL_TY;
     private  String X_SWIFI_SVC_SE;
@@ -22,12 +23,13 @@ public class WifiInfoDTO {
     private  String LAT;
     private  String WORK_DTTM ;
 
-    public WifiInfoDTO(String x_SWIFI_MGR_NO, String x_SWIFI_MAIN_NM, String x_SWIFI_WRDOFC, String x_SWIFI_ADRES1, String x_SWIFI_ADRES2, String x_SWIFI_INSTL_MBY, String x_SWIFI_INSTL_TY, String x_SWIFI_SVC_SE, String x_SWIFI_CMCWR, String x_SWIFI_CNSTC_YEAR, String x_SWIFI_INOUT_DOOR, String x_SWIFI_REMARS3, String LNT, String LAT, String WORK_DTTM) {
+    public WifiInfoDTO(String x_SWIFI_MGR_NO, String x_SWIFI_MAIN_NM, String x_SWIFI_WRDOFC, String x_SWIFI_ADRES1, String x_SWIFI_ADRES2,String x_SWIFI_INSTL_FLOOR, String x_SWIFI_INSTL_MBY, String x_SWIFI_INSTL_TY, String x_SWIFI_SVC_SE, String x_SWIFI_CMCWR, String x_SWIFI_CNSTC_YEAR, String x_SWIFI_INOUT_DOOR, String x_SWIFI_REMARS3, String LNT, String LAT, String WORK_DTTM) {
         this.X_SWIFI_MGR_NO = x_SWIFI_MGR_NO;
         this.X_SWIFI_MAIN_NM = x_SWIFI_MAIN_NM;
         this.X_SWIFI_WRDOFC = x_SWIFI_WRDOFC;
         this.X_SWIFI_ADRES1 = x_SWIFI_ADRES1;
         this.X_SWIFI_ADRES2 = x_SWIFI_ADRES2;
+        this.X_SWIFI_INSTL_FLOOR =x_SWIFI_INSTL_FLOOR;
         this.X_SWIFI_INSTL_MBY = x_SWIFI_INSTL_MBY;
         this.X_SWIFI_INSTL_TY = x_SWIFI_INSTL_TY;
         this.X_SWIFI_SVC_SE = x_SWIFI_SVC_SE;
